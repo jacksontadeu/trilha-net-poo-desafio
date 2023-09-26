38c063f4e97e0df9a8d93efad35b1ea212e3f347
Jackson Moraes <jacksontadeu@hotmail.com>
2023-09-26 14:18:17 -0300

    Resolução do desafio DIO trilha .Net

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,23 @@
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo {  get; set; }
+        protected string IMEI { get; set; }
+        protected int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei,int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -19,6 +28,17 @@ namespace DesafioPOO.Models
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+        }
+       
+       
+        public override string ToString()
+        {
+            StringBuilder texto = new StringBuilder();
+            texto.AppendLine($"Modelo.: {Modelo}");
+            texto.AppendLine($"IMEI...: {IMEI}");
+            texto.AppendLine($"Número.: {Numero}");
+            texto.Append($"Memória: {Memoria}");
+            return texto.ToString();
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone iphone = new Iphone("99111-1111", "Iphone 11", "123456789", 128);
+Console.WriteLine(iphone.ToString());
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
+Console.WriteLine();
+Console.WriteLine("==============================================");
+Console.WriteLine();
+Nokia nokia = new Nokia("99222-2222", "Nokia X-11", "987654321", 64);
+Console.WriteLine(nokia.ToString());
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("TIK-TOK");
+Console.WriteLine("==============================================");
